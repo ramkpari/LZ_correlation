5a6a974ddc86099f7da0b406d2f69377200d62aa
Ram <ramsbam@gmail.com>
2021-01-25 02:27:25 +0000

    Verification run for updatd exp

--- new_STLI_updated_lastrun.py
+++ new_STLI_updated_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2020.2.10),
-    on January 15, 2021, at 02:03
+    on January 25, 2021, at 02:03
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
